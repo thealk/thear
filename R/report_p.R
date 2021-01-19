6383cc2045b6e326f3f476e40e020c7f55d43f52
thealk <thea.knowles@gmail.com>
2021-01-19 10:27:42 -0500

    Update report_p.R

--- R/report_p.R
+++ R/report_p.R
@@ -3,7 +3,7 @@
 
 report_p <- function(x){
   return(ifelse(x<0.001,
-                "<.001",
+                "< .001",
                 paste0("= ",APAstyler::snip(round(x,3),1))
                 #scales::pvalue(x,add_p = TRUE),1)))
                 #paste0("= ",as.character(round(x,3)))
