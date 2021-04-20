57d47ffba2f90060b3debe95836a6920d8f33a81
Thea Knowles <thea.knowles@gmail.com>
2021-04-20 16:52:23 -0400

    added effect sizes to thear::make_coefs()

--- R/make_coefs.R
+++ R/make_coefs.R
@@ -9,13 +9,22 @@ make_coefs <- function(model, mod_name){
     mutate_if(is.numeric,round,3) %>%
     column_to_rownames()
 
+  # Get total variance in order to calculate effect size
+  mod_var <- summary(model)$varcor %>% as.data.frame()
+  tot_var <- sum(mod_var$vcov)
+
+  df <- df %>%
+    mutate(eff_size = Estimate/sqrt(tot_var))
+
   b <- df['Estimate']
   p <- df['Pr(>|t|)']
+  d <- df['eff_size']
 
   b_name <- paste0(mod_name,"_B")
-
   p_name <- paste0(mod_name,"_P")
+  d_name <- paste0(mod_name,"_D")
 
   assign(b_name, b, envir=parent.frame())
   assign(p_name, p, envir=parent.frame())
+  assign(d_name, d, envir=parent.frame())
 }
