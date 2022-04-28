24a8e42ad21e2bb3825859605c78c79aeb71e35e
Thea Knowles <thea.knowles@gmail.com>
2022-04-27 20:06:36 -0400

    update report_p
    used APAstyler which is now deprecated. Will deal with leading 0s another day.

--- R/report_p.R
+++ R/report_p.R
@@ -4,7 +4,9 @@
 report_p <- function(x){
   return(ifelse(x<0.001,
                 "< .001",
-                paste0("= ",APAstyler::snip(round(x,3),1))
+                paste0("= ",round(x,3),1)
+                # APAstyler is now deprecated
+                #paste0("= ",APAstyler::snip(round(x,3),1))
                 #scales::pvalue(x,add_p = TRUE),1)))
                 #paste0("= ",as.character(round(x,3)))
   ))
